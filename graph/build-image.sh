6e178826a0a72863455a1bb42ab6aa1907e4ba5d
alizeefan <34604492+alizeefan@users.noreply.github.com>
2017-12-16 23:43:36 +0100

    Update build-image.sh

--- graph/build-image.sh
+++ graph/build-image.sh
@@ -1,5 +1,4 @@
-curl -o Dockerfile https://raw.githubusercontent.com/alizeefan/runtime/master/graph/Dockerfile
-sudo docker build -t neo4j-apoc 
+sudo docker build -t neo4j-apoc https://raw.githubusercontent.com/alizeefan/runtime/master/graph/Dockerfile
 sudo mkdir -p /neo4j/data
 sudo chmod 777 /neo4j/data
 sudo docker run -dit --restart unless-stopped --publish=7474:7474 --publish=7687:7687 --volume=/neo4j/data:/data --env NEO4J_AUTH=none neo4j-apoc
