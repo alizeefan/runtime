0134626ee5c0ad4c40b27c620ec05ae03eda9677
alizeefan <34604492+alizeefan@users.noreply.github.com>
2017-12-16 20:33:07 +0100

    Update build-image.sh

--- graph/build-image.sh
+++ graph/build-image.sh
@@ -1,5 +1,5 @@
 curl -o Dockerfile https://raw.githubusercontent.com/alizeefan/runtime/master/graph/Dockerfile
-sudo docker build -t neo4j-apoc .
+sudo docker build -t neo4j-apoc 
 sudo mkdir -p /neo4j/data
 sudo chmod 777 /neo4j/data
 sudo docker run -dit --restart unless-stopped --publish=7474:7474 --publish=7687:7687 --volume=/neo4j/data:/data neo4j-apoc
