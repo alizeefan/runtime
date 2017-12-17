5a5cc1cd858f734169b57bb54967a84e02056e7d
alizeefan <34604492+alizeefan@users.noreply.github.com>
2017-12-17 19:46:45 +0100

    Update prepare_host.sh

--- host/prepare_host.sh
+++ host/prepare_host.sh
@@ -2,6 +2,6 @@ sudo apt-get update
 sudo apt-get -y upgrade
 sudo apt-get install apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu zesty stable"
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release --codename | cut -f2) stable"
 sudo apt-get update
 sudo apt-get -y install docker-ce
