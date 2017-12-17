698cb97726c632269d162322c69c92700ca80b65
alizeefan <34604492+alizeefan@users.noreply.github.com>
2017-12-17 16:15:56 +0100

    Update build-image.sh

--- graph/build-image.sh
+++ graph/build-image.sh
@@ -1,4 +1,11 @@
 sudo docker build -t neo4j-apoc https://raw.githubusercontent.com/alizeefan/runtime/master/graph/Dockerfile
-sudo mkdir -p /neo4j/data
-sudo chmod 777 /neo4j/data
-sudo docker run -dit --restart unless-stopped --publish=7474:7474 --publish=7687:7687 --volume=/neo4j/data:/data --env NEO4J_AUTH=none neo4j-apoc
+sudo mkdir -p /neo4j/data /neo4j/logs /neo4j/import
+sudo chmod 777 /neo4j/data /neo4j/logs /neo4j/import
+sudo docker run -dit --restart unless-stopped \
+  --publish=7474:7474 --publish=7687:7687 \
+   --name=neo4j \
+   --volume=/neo4j/data:/var/lib/neo4j/data \
+   --volume=/neo4j/logs:/var/lib/neo4j/logs \
+   --volume=/neo4j/import:/var/lib/neo4j/import \
+   --env NEO4J_AUTH=none \
+   neo4j-apoc
